be159762b2780747bf005e0a1db96129ff6037a2
pebebtol <julius.felchow@googlemail.com>
2016-11-18 13:07:36 +0100

    added a autosetup for the links...

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+ln -s ~/.dotfiles/asciii.yml ~/.asciii.yml
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gvimrc ~/.gvimrc
+ln -s ~/.dotfiles/helligkeit.sh ~/.helligkeit.sh
+ln -s ~/.dotfiles/helligkeit1.sh ~/.helligkeit1.sh
+ln -s ~/.dotfiles/i3barconfig ~/.config/i3status/config
+ln -s ~/.dotfiles/i3config ~/.config/i3/config
+ln -s ~/.dotfiles/magnify.sh ~/.magnify.sh
+ln -s ~/.dotfiles/mic ~/.mic
+ln -s ~/.dotfiles/quickmarksvimp ~/.vimperator/info/default/quickmarks
+ln -s ~/.dotfiles/rc.conf ~/.config/ranger/rc.conf
+ln -s ~/.dotfiles/vimperatorrc ~/.vimperatorrc
+ln -s ~/.dotfiles/Xresource ~/.Xresource
+ln -s ~/.dotfiles/zshrc ~/.zshrc
